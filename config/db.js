b8c9f451f9c750d376c09942efdcc344655b00b0
Ekant-Yash-Kashyap <yash.kashyap@ekant.in>
2025-07-18 23:02:18 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -11,8 +11,8 @@ export const db = mysql.createPool({
 try {
   const connection = await db.getConnection();
   //   console.log("✅ Database connected successfully.");
-  connection.release(); // important to release back to pool
+  connection.release(); 
 } catch (err) {
   console.error("❌ Database connection failed:", err);
-  process.exit(1); // optional: stop server if DB is essential
+  process.exit(1); 
 }
